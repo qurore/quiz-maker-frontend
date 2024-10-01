0a932447ece4373b4e1a47cd2ee91711f3d02cdd
Ryosuke Shiroshita <admin@qurore.com>
2024-09-30 18:55:21 -0700

    added review button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { IoMdArrowDropdown } from 'react-icons/io';
+import { RiFileListLine } from 'react-icons/ri';
 
 function Navbar() {
   return (
     <nav className="border-b border-gray-200 shadow-sm">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold text-blue-600">Quiz Maker</Link>
-        <div className="relative">
+        <Link to="/" className="text-xl font-bold">Quiz Maker</Link>
+        <div className="flex items-center space-x-4">
+          <Link to="/review" className="text-gray-600 hover:text-blue-600 transition-colors focus:outline-none flex items-center">
+            <RiFileListLine className="h-6 w-6" />
+            <span className="ml-1 mr-2">Review</span>
+          </Link>
           <button className="text-gray-600 hover:text-blue-600 transition-colors focus:outline-none flex items-center">
             <FiSettings className="h-6 w-6" />
             <IoMdArrowDropdown className="h-4 w-4 ml-1" />
