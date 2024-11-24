39b7bb5755c9349cb9b256a59afce9ebd632ccce
Ryosuke Shiroshita <admin@qurore.com>
2024-11-23 23:29:48 -0800

    modified search placeholder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ function Navbar() {
               type="text"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Search technical term..."
+              placeholder="Search anything you want to know..."
               className="w-full px-4 py-2 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
             <button
